eb273c3a0af65171eefa7000e66daead3ae3e939
JustCati <ale.catinello@gmail.com>
2024-05-26 20:19:11 +0200

    feat: mode switch arg

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-from tools.train_net import main
+import os
+import sys
+sys.path.append(os.path.join(os.path.dirname(os.path.abspath(__file__)), "src"))
+
+from tools.train_net import train
 from rekognition_online_action_detection.utils.parser import load_cfg
 
 
@@ -6,8 +10,10 @@ from rekognition_online_action_detection.utils.parser import load_cfg
 
 
 def main(cfg):
-    ...
-
+    if cfg.TRAIN:
+        train(cfg)
+    else:
+        raise NotImplementedError("Test is not implemented yet.")
 
 
 
