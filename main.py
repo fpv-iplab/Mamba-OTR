d14206f3c07c1490483fba96d6420973a4802daf
JustCati <ale.catinello@gmail.com>
2024-05-26 13:36:42 +0200

    chore(train): create main

---
+++ main.py
@@ -0,0 +1,16 @@
+from tools.train_net import main
+from rekognition_online_action_detection.utils.parser import load_cfg
+
+
+
+
+
+def main(cfg):
+    ...
+
+
+
+
+
+if __name__ == "__main__":
+    main(load_cfg())
