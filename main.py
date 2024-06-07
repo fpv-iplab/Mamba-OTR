297b5361af39024cdd2c16508ef10e384b5ea3e5
JustCati <ale.catinello@gmail.com>
2024-06-07 10:31:16 +0200

    feat: target existance check

--- main.py
+++ main.py
@@ -5,17 +5,22 @@ sys.path.append(os.path.join(os.path.dirname(os.path.abspath(__file__)), "src"))
 from tools.test_net import test
 from tools.train_net import train
 from rekognition_online_action_detection.utils.parser import load_cfg
+from rekognition_online_action_detection.utils.actionstartend_utils import thumos_target_perframe_to_actionstartend
 
 
 
 
 
 def main(cfg):
+    targetPath = os.path.join(cfg.DATA.DATA_ROOT, cfg.INPUT.TARGET_PERFRAME)
+    if not os.path.exists(targetPath) or len(os.listdir(targetPath)) == 0:
+        task = "start" in targetPath
+
+        inputPath = targetPath.replace("start_", "") if task else targetPath.replace("end_", "")
+        thumos_target_perframe_to_actionstartend(inputPath, targetPath, type="start" if task else "end")
+
     if cfg.TRAIN:
         train(cfg)
-    else:
-        test(cfg)
-
 
 
 
