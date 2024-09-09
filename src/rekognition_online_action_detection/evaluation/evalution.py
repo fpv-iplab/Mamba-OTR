a4055cc8a8dba78bc3059898a6026384f2cd96df
JustCati <ale.catinello@gmail.com>
2024-09-09 12:29:38 +0200

    fix: default ignore index int to list

--- src/rekognition_online_action_detection/evaluation/evalution.py
+++ src/rekognition_online_action_detection/evaluation/evalution.py
@@ -14,7 +14,7 @@ compute_result = Registry()
 @compute_result.register("perpoint")
 def eval_perframe(cfg, ground_truth, prediction, **kwargs):
     class_names = kwargs.get('class_names', cfg.DATA.CLASS_NAMES)
-    ignore_index = kwargs.get('ignore_index', cfg.DATA.IGNORE_INDEX)
+    ignore_index = kwargs.get('ignore_index', [cfg.DATA.IGNORE_INDEX])
     metrics = kwargs.get('metrics', cfg.DATA.METRICS)
     postprocessing = kwargs.get('postprocessing', default_pp(cfg.DATA.DATA_NAME))
     return perframe_perpoint_average_precision(
@@ -29,7 +29,7 @@ def eval_perframe(cfg, ground_truth, prediction, **kwargs):
 @compute_result.register('perframe')
 def eval_perframe(cfg, ground_truth, prediction, **kwargs):
     class_names = kwargs.get('class_names', cfg.DATA.CLASS_NAMES)
-    ignore_index = kwargs.get('ignore_index', cfg.DATA.IGNORE_INDEX)
+    ignore_index = kwargs.get('ignore_index', [cfg.DATA.IGNORE_INDEX])
     metrics = kwargs.get('metrics', cfg.DATA.METRICS)
     postprocessing = kwargs.get('postprocessing', default_pp(cfg.DATA.DATA_NAME))
     return perframe_average_precision(
