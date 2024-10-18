dfae2e54309ed788e428cdfcb64ad783ee4aef70
JustCati <ale.catinello@gmail.com>
2024-10-18 14:37:10 +0200

    feat: generate target files

--- tools/generate_targets_enigma.py
+++ tools/generate_targets_enigma.py
@@ -75,13 +75,35 @@ def generate_features(ann_data, env, out_path):
             np.save(os.path.join(out_path, f"{video}.npy"), data)
 
 
+def generate_targets(ann_data, out_path):
+    data = dict.fromkeys(ann_data["videos"].keys())
+    for video in ann_data["videos"]:
+        frame_count = ann_data["videos"][video]["frame_count"]
+        data[video] = np.zeros((frame_count, len(list(ann_data["interaction_types"].keys())) + 1))
+
+    for frame in ann_data["frame_annotations"]:
+        video_id = frame.split("_")[0]
+        frame_num = int(frame.split("_")[1])
+        for interaction in ann_data["frame_annotations"][frame]["interactions"]:
+            action = interaction["interaction_category"]
+            data[video_id][frame_num, action + 1] = 1   # + 1 because 0 is reserved for background
+
+    for video in data:
+        indexis = np.where(~data[video].any(axis=1))[0]
+        data[video][indexis, 0] = 1
+
+    for video in data:
+        np.save(os.path.join(out_path, video + ".npy"), data[video])
+
+
+
 
 
 def main(args):
     ENIGMA_VIDEO_COUNT = 51
     data_path = args.data_path
     if not os.path.exists(data_path):
-        raise FileNotFoundError("Data path does not exist")
+        raise FileNotFoundError(f"Data path {data_path} does not exist")
 
     ann_file_path = os.path.join(data_path, "annotations_raw.json")
     if not os.path.exists(ann_file_path):
@@ -90,9 +112,10 @@ def main(args):
     feature_file_path = os.path.join(data_path, "raw_data")
     if not os.path.exists(feature_file_path):
         raise FileNotFoundError("Feature file does not exist")
-    
-    ann_out_path = os.path.join(data_path, "annotations_fix.json")
+
     feature_out_path = os.path.join(data_path, "DINOv2")
+    ann_out_path = os.path.join(data_path, "annotations_fix.json")
+    target_path = os.path.join(data_path, "target_perframe")
 
 
     with open(ann_file_path, "r") as f:
@@ -107,13 +130,16 @@ def main(args):
     if len(os.listdir(feature_out_path)) != ENIGMA_VIDEO_COUNT:
         generate_features(ann_data, env, feature_out_path)
     
-    
+    if not os.path.exists(target_path):
+        os.makedirs(target_path)
+    if len(os.listdir(target_path)) != ENIGMA_VIDEO_COUNT:
+        generate_targets(ann_data, target_path)
 
 
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Generate targets for ENIGMA-51 Dataset')
-    parser.add_argument('--data-path', type=str, default=os.path.join(os.path.abspath(__file__), "data", "ENIGMA-51"))
+    parser.add_argument('--data-path', type=str, default=os.path.join(os.path.dirname(__file__), "..", "data",  "ENIGMA"))
     args = parser.parse_args()
     main(args)
