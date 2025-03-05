fix: simplify entropy calculation in FocalLoss implementation

src/rekognition_online_action_detection/criterions/criterions.py
@@ -216,8 +216,7 @@ class FocalLoss(nn.Module):
 
 
     def __entropy(self, p: torch.Tensor) -> torch.Tensor:
-        # h = -torch.sum(p * torch.log(p + 1e-6), dim=1)
-        h = -torch.sum(p * torch.log(p + 1e-6) + (1 - p) * torch.log(1 - p + 1e-6), dim=1)
+        h = -torch.sum(p * torch.log(p + 1e-6), dim=1)
         return h.mean()
 
 